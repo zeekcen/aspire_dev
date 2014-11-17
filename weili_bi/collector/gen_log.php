375fc9492f9ae6b98a674e96cee37d91c0cf4392
zeekcen <zeekcen@gmail.com>
2014-11-17 18:19:42 +0800

    删除cookie和添加枚举类映射id

--- weili_bi/collector/gen_log.php
+++ weili_bi/collector/gen_log.php
@@ -21,7 +21,7 @@ $interact_arr = array(
 		'关键字'
 		);
 
-// 访问url类型
+// 访问url
 $visit_url_arr = array(
 		'http://www.36kr.com/p/217017.html?ref=index_feature_topics',
 		'http://www.primesplus.com/zh/',
@@ -119,27 +119,26 @@ $start_time = "2014-11-01 00:05:00";
 $stamp = strtotime($start_time);
 $now = time() - 11*86400;
 
-$branch = '02';
+$branch = '2';
 $merchant_id = '0769';
 $merchant_code = 'dg';
 
 do {
 	$datetime = date('Y-m-d H:i:00', $stamp);
 	//echo $datetime."\n";
-	$stamp = $stamp + 10*60;
+	$stamp = $stamp + 1*2;
 	$user_id = mt_rand(1, 99999);
 	$open_id = md5($user_id);
 	$phone = str_pad($user_id, 11, '1', STR_PAD_LEFT);
 	$ctime = date('YmdHis', $stamp);
 	$date = date('Ymd', $stamp);
-	$cookie = substr($open_id, 0, 20);
 	$visit_src = mt_rand(1,2); // 1.内部访问;2.外部访问
 	$user_type = mt_rand(0,1); 
 	// 生成用户操作日志
 	$idx = mt_rand(0, 5);
-	$opt = $opt_type_arr[$idx];
+	$opt = $idx + 1;
 	$idx = mt_rand(0, 6);
-	$act = $interact_arr[$idx];
+	$act = $idx + 1;
 	$log_arr = array($branch, $merchant_id, $merchant_code, $user_id, $open_id, $phone, $opt, $ctime, $act);
 	$line = implode('|', $log_arr);
 	write_log($merchant_code, 'operate', $line, $date);
@@ -147,20 +146,20 @@ do {
 	$idx = mt_rand(0, 3);
 	$page_title = $page_title_arr[$idx];
 	$visit_url = $visit_url_arr[$idx];
-	$log_arr = array($branch, $merchant_id, $merchant_code, $user_id, $open_id, $phone, $cookie, $user_type, $visit_src, $page_title, $visit_url, $ctime);
+	$log_arr = array($branch, $merchant_id, $merchant_code, $user_id, $open_id, $phone, $user_type, $visit_src, $page_title, $visit_url, $ctime);
 	$line = implode('|', $log_arr);
 	write_log($merchant_code, 'visit', $line, $date);
 	// 生成活动记录日志
 	$idx = mt_rand(0, 2);	
 	$act_id = $idx + 1;
-	$act = $act_arr[$idx];
+	$act = $idx + 1;
 	$act_url = $act_url_arr[$idx];
 	$act_title = $act_title_arr[$idx];
 	$status = mt_rand(0, 2);
 	$send_user_id = mt_rand(1, 99999);
 	$rece_user_id = mt_rand(1, 99999);
 	$share_num = uniqid();
-	$log_arr = array($branch, $merchant_id, $merchant_code, $user_id, $user_type, $open_id, $phone, $cookie, $visit_src, $act_id, $act_url, $act_title, $act, $status, $send_user_id, $rece_user_id, $share_num, $ctime);
+	$log_arr = array($branch, $merchant_id, $merchant_code, $user_id, $user_type, $open_id, $phone, $visit_src, $act_id, $act_url, $act_title, $act, $status, $send_user_id, $rece_user_id, $share_num, $ctime);
 	$line = implode('|', $log_arr);
 	write_log($merchant_code, 'activity', $line, $date);
 	// 生成菜单统计日志
